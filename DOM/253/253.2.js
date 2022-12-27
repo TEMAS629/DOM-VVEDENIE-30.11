3fe8a6b65cc5c465eee2f1c25480e837f95f3678
TEMAS629 <94691533+TEMAS629@users.noreply.github.com>
2022-12-27 13:27:22 +0300

    Add files via upload

---
+++ DOM/253/253.2.js
@@ -0,0 +1,8 @@
+console.log(document.querySelector('#image').src);
+console.log(document.querySelector('#image').width);
+console.log(document.querySelector('#image').height);
+
+let image = document.querySelector('#image');
+image.src = 'avatar.png';
+image.width  = '300';
+image.height  = '500';
