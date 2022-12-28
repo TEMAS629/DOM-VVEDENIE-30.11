7f2cb6d9e737a817215f5710a8b2616ccf4575a9
TEMAS629 <94691533+TEMAS629@users.noreply.github.com>
2022-12-28 16:05:38 +0300

    Delete DOM/255 directory

--- DOM/255/1.js
+++
@@ -1,8 +0,0 @@
-let elems = document.querySelectorAll('.www');
-elems.addEventListener('click', func);
-
-function func() {
-  for (let elem of elems) {
-  	elem.textContent = elem.textContent + '!';
-  };
-};
