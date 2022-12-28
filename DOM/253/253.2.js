aa2a836317753bd7b96db910b349c855d4aa7806
TEMAS629 <94691533+TEMAS629@users.noreply.github.com>
2022-12-28 16:05:21 +0300

    Delete DOM/253 directory

--- DOM/253/253.2.js
+++
@@ -1,8 +0,0 @@
-console.log(document.querySelector('#image').src);
-console.log(document.querySelector('#image').width);
-console.log(document.querySelector('#image').height);
-
-let image = document.querySelector('#image');
-image.src = 'avatar.png';
-image.width  = '300';
-image.height  = '500';
